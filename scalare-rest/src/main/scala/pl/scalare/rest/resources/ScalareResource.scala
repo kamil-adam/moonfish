283ff5d82773eec9c7f00990bc086f80f2b6f711
Kamil Adam <kamil.adam.zabinski@gmail.com>
2017-02-07 20:18:32 +0100

    .travis.yml

--- scalare-rest/src/main/scala/pl/scalare/rest/resources/ScalareResource.scala
+++ scalare-rest/src/main/scala/pl/scalare/rest/resources/ScalareResource.scala
@@ -2,11 +2,14 @@ package pl.scalare.rest.resources
 
 import java.util.Optional
 import java.util.concurrent.atomic.AtomicLong
+import javax.annotation.security.RolesAllowed
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.{GET, Path, Produces, QueryParam}
 
 import com.codahale.metrics.annotation.Timed
+import io.dropwizard.auth.Auth
 import pl.scalare.japi.Information
+import pl.scalare.rest.User
 
 @Path("/scalare")
 @Produces(Array(MediaType.APPLICATION_JSON))
@@ -23,4 +26,9 @@ class ScalareResource(val template: String, val defaultName: String) {
     return new Information(counter.incrementAndGet(), value);
 
   }
+
+  @RolesAllowed(Array("ADMIN"))
+  @GET
+  @Path("/user")
+  def getSecretPlan(@Auth  user: User) = user
 }
