db684eabfe6c9a55c7863bffe8f00331f9170939
Kamil Adam <kamil.adam.zabinski@gmail.com>
2017-03-04 11:22:26 +0100

    test for on2ml

--- scalare-util/src/test/scala/pl/scalare/util/asap/Json2YamlSpec.scala
+++ scalare-util/src/test/scala/pl/scalare/util/asap/Json2YamlSpec.scala
@@ -26,6 +26,10 @@ class Json2YamlSpec extends FunSpec with GivenWhenThen {
         val yaml = onml.applyTry("[0]")
         assert("---\n- 0\n" == yaml)
       }
+      it("should produce array 2 when consume array 2") {
+        val yaml = onml.applyTry("[0,1]")
+        assert("---\n- 0\n- 1\n" == yaml)
+      }
       it("should produce object when consume empty object") {
         val yaml = onml.applyTry("{}")
         assert("--- {}\n" == yaml)
@@ -33,6 +37,18 @@ class Json2YamlSpec extends FunSpec with GivenWhenThen {
       it("should produce object when consume object") {
         val yaml = onml.applyTry("{\"a\":0}")
         assert("---\na: 0\n" == yaml)
+      }
+      it("should produce object 2 when consume object 2") {
+        val yaml = onml.applyTry("{\"a\":0, \"b\":1}")
+        assert("---\na: 0\nb: 1\n" == yaml)
+      }
+      it("should produce object array when consume object array") {
+        val yaml = onml.applyTry("[{}]")
+        assert("---\n- {}\n" == yaml)
+      }
+      it("should produce array object when consume array object") {
+        val yaml = onml.applyTry("{\"a\":[]}")
+        assert("---\na: []\n" == yaml)
       }
     }
   }
