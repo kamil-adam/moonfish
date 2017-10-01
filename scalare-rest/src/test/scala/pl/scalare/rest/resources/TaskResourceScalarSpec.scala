720f4c4167427e37457f4dc7a12d75e5c9cee4b1
Kamil Adam <enkidu616@gmail.com>
2017-10-01 20:44:51 +0200

    add TaskResourceScalarSpec

--- scalare-rest/src/test/scala/pl/scalare/rest/resources/TaskResourceScalarSpec.scala
+++ scalare-rest/src/test/scala/pl/scalare/rest/resources/TaskResourceScalarSpec.scala
@@ -1,16 +1,16 @@
 package pl.scalare.rest.resources
 
-import pl.scalare.impl.repo.SelectRepoFake
+import pl.scalare.impl.repo.TaskRepoFake
 import pl.scalare.rest.ViewConfiguration
 import pl.scalare.spec.GrayScalarSpec
 
-class DatabaseResourceScalarSpec extends GrayScalarSpec {
+class TaskResourceScalarSpec extends GrayScalarSpec {
   describe("A DatabaseResourceScalarSpec") {
-    val repo = new SelectRepoFake()
+    val repo = new TaskRepoFake()
     val conf = new ViewConfiguration
-    val rsrc = new DatabaseResource(repo, conf)
+    val rsrc = new TaskResource(repo, conf)
     it("when invoke databases") {
-      rsrc.databases.length should be > 0
+      rsrc.tasks.length should be > 0
     }
   }
 }
