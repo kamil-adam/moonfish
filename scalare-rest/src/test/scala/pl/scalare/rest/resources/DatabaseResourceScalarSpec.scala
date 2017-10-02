e4d513ff7c0b41d409cf44586634dc7f68baff48
Kamil Adam <enkidu616@gmail.com>
2017-10-02 20:56:07 +0200

    DatabaseResourceScalarSpec

--- scalare-rest/src/test/scala/pl/scalare/rest/resources/DatabaseResourceScalarSpec.scala
+++ scalare-rest/src/test/scala/pl/scalare/rest/resources/DatabaseResourceScalarSpec.scala
@@ -1,7 +1,10 @@
 package pl.scalare.rest.resources
 
+import java.util.Optional
+
 import pl.scalare.impl.repo.SelectRepoFake
 import pl.scalare.rest.ViewConfiguration
+import pl.scalare.rest.views.KeysView
 import pl.scalare.spec.GrayScalarSpec
 
 class DatabaseResourceScalarSpec extends GrayScalarSpec {
@@ -15,6 +18,28 @@ class DatabaseResourceScalarSpec extends GrayScalarSpec {
     it("when invoke databasesView") {
       val view = rsrc.databasesView
       view.databases.length should be > 0
+    }
+
+    it("when invoke keys") {
+      rsrc.databases.foreach {
+        rsrc.keys(_).length should be > 0
+      }
+    }
+    it("when invoke keysView") {
+      rsrc.databases.foreach { key =>
+          val view: KeysView = rsrc.keysView(key)
+          view.keys.length should be > 0
+      }
+    }
+    it("when invoke select") {
+      val databases = Array("sqlite", "hsql","h2")
+      databases.foreach { db =>
+        rsrc.keys(db).foreach { key =>
+          val select = rsrc.select(db, key, Optional.empty(), Optional.empty())
+          select.getHeader.length should be >= 0
+          select.getBody.length should be >= 0
+        }
+      }
     }
   }
 }
